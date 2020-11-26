f37908402aa85dfed6731bc0aa5ce195b1584d1a
bobajeff <bobajeff82@gmail.com>
2020-11-26 02:14:18 -0600

    Fixed filter for visible text

--- connect_to_chrome.js
+++ connect_to_chrome.js
@@ -7,10 +7,21 @@ getpagetext = async function (cdp){
     var document = await cdp.send('DOMSnapshot.captureSnapshot', { computedStyles: ['visibility'], includeDOMRects: true });
     var {parentIndex, nodeType, nodeName, nodeValue, attributes} = document['documents'][0]['nodes'];
     var {styles, nodeIndex, clientRects} = document['documents'][0]['layout'];
-    //console.log(document['strings']);
-    console.log(document['documents'][0]['layout']);
+    // console.log('strings');
+    // console.log(document['strings']);
+    //console.log(document['documents'][0]['layout']);
     //console.log(styles[1]);
     //console.log(document['documents'][0]);
+    // console.log('nodeValue');
+    // console.log(nodeValue);
+    // console.log('nodeName');
+    // console.log(nodeName);
+    // console.log('clientRects');
+    // console.log(clientRects);
+    // console.log('parentIndex');
+    // console.log(parentIndex);
+    // console.log('nodeIndex');
+    // console.log(nodeIndex);
 
     var str_array = []
     nodeType.forEach(async (node_type, itr) => {
@@ -27,47 +38,30 @@ getpagetext = async function (cdp){
             if (node_type == 3){ // check if node is text type
                 if (/[^\s]/m.test(value_string))  // remove any tags that contain only whitespace
                 {
-                    var node_is_visible = false;
-                    nodeIndex.forEach(async(node_index_value, styles_string_key) => {
-                        
-                        var parent_of_style_key = parentIndex[styles_string_key];
 
-                        var parent_client_rect_property = clientRects[parent_of_style_key];
-                        str_array.push(parent_client_rect_property);
-                        if (parent_client_rect_property != undefined)
+                    nodeIndex.forEach(async(node_index_value, node_index_key) => {
+                        //check if the parent index points to the current node index
+                        //if so the values of the corresponding clientrects can be access by the current index key
+                        if (parent_index == node_index_value) 
                         {
-                            //                             width                                height
-                            if (!!parent_client_rect_property[2] || !!parent_client_rect_property[3] || !!parent_client_rect_property.length){
-                                node_is_visible = true;
+                            var parent_client_rect_property = clientRects[node_index_key];
+                            if (parent_client_rect_property != undefined)
+                            {
+                                //                             width                                height
+                                if (!!parent_client_rect_property[2] || !!parent_client_rect_property[3] || !!parent_client_rect_property.length){
+                                    //replace many whitespace characters with one space as thats more likely 
+                                    /* attribute_strings.filter(attribute_string => attribute_string == "aria-hidden");
+                                    str_array.push('hidden'); */
+                                    var text_sanitized = value_string.replace(/\s+/g, " ");
+                                    str_array.push(text_sanitized);
+                                }
                             }
-                            
-                        }
-                        else
-                        {
-                            node_is_visible = false;
-                        }
-                        //figure out which nodes get styles.
-                        if(node_index_value == itr)
-                        {
-                            //check the visibility property
-                            var visibility = document['strings'][styles[styles_string_key][0]]; //index 0 is visibility property here because it's the first property specified on computedStyles
-                            //str_array.push('visibility: ' + visibility);
 
-                            //var parentIndex[node_index_value]
-                            //var clientRect_length = clientRects[styles_string_key].length;
-                            
                         }
+
                     });
                     
-                    if (node_is_visible)
-                    {
-                        str_array.push(value_string.replace(/\s+/g, " "));
-
-                    }
                     
-                    //replace many whitespace characters with one space as thats more likely 
-                    /* attribute_strings.filter(attribute_string => attribute_string == "aria-hidden");
-                    str_array.push('hidden'); */
                 }
             }
         }
