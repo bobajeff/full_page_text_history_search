2dba9000dd5d0c9d4e885f290ea7affa96572e6b
bobajeff <bobajeff82@gmail.com>
2020-12-31 09:43:44 -0600

    switched from puppeteer to puppeteer-core to help with install time

--- connect_to_chrome.js
+++ connect_to_chrome.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 import getWebSocketDebuggerUrl from './getWebSocketDebuggerUrl.js';
 import runPageOperations from './page_operations.js';
